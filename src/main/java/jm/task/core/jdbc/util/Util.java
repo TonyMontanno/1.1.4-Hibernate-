ab7032dabac7868e45a3414e84c9b382f5cbdbdc
TonyMontanno <Crazy_antonio@mail.ru>
2022-08-03 01:08:30 +0300

    Create Dao and Service 3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,6 @@
 package jm.task.core.jdbc.util;
 
 
-import jm.task.core.jdbc.model.User;
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,9 +10,9 @@ public class Util {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/Project";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "obraz89nn";
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
     private static Connection connection;
-    private static SessionFactory sessionFactory = null;
+
 
 
     public static Connection getConnection() {
@@ -30,24 +24,4 @@ public class Util {
         return connection;
     }
 
-    public static SessionFactory getSessionFactory() {
-        try {
-            Configuration configuration = new Configuration()
-                    .setProperty("hibernate.connection.driver_class", DRIVER)
-                    .setProperty("hibernate.connection.url", URL)
-                    .setProperty("hibernate.connection.username",USERNAME )
-                    .setProperty("hibernate.connection.password", PASSWORD)
-                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-                    .addAnnotatedClass(User.class)
-                    .setProperty("hibernate.c3p0.min_size", "5")
-                    .setProperty("hibernate.c3p0.max_size", "200")
-                    .setProperty("hibernate.c3p0.max_statements", "200");
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return sessionFactory;
-    }
-}
+   }
